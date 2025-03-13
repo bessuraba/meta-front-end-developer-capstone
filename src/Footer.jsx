df027a92c4867f9e5508358f969a707a25cba42a
Slava Varda <slawa.bessuraba@gmail.com>
2025-03-13 20:46:07 +0100

    semantic

---
+++ src/Footer.jsx
@@ -0,0 +1,57 @@
+const Footer = () => (
+  <footer>
+    <div>
+      <img src="/Logo.svg" alt="logo"/>
+      <div>
+        <h6>Doormat Navigation</h6>
+        <ul>
+          <li>
+            <a href="/" title="Home">Home</a>
+          </li>
+          <li>
+            <a href="/about" title="About">About</a>
+          </li>
+          <li>
+            <a href="/menu" title="Menu">Menu</a>
+          </li>
+          <li>
+            <a href="/reservation" title="Reservation">Reservation</a>
+          </li>
+          <li>
+            <a href="/order" title="Order online">Order online</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h6>Contact</h6>
+        <ul>
+          <li>
+            Address
+          </li>
+          <li>
+            Email
+          </li>
+          <li>
+            Phone
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h6>Social</h6>
+        <ul>
+          <li>
+            Instagram
+          </li>
+          <li>
+            Facebook
+          </li>
+          <li>
+            LinkedIn
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+)
+
+export default Footer
---
+++ src/Nav.jsx
@@ -0,0 +1,29 @@
+const Nav = () => (
+  <nav>
+    <div>
+      <img src="/Logo.svg" alt="logo"/>
+      <ul role="navigation">
+        <li>
+          <a href="/" title="Home">Home</a>
+        </li>
+        <li>
+          <a href="/about" title="About">About</a>
+        </li>
+        <li>
+          <a href="/menu" title="Menu">Menu</a>
+        </li>
+        <li>
+          <a href="/reservation" title="Reservation">Reservation</a>
+        </li>
+        <li>
+          <a href="/order" title="Order online">Order online</a>
+        </li>
+        <li>
+          <a href="/login" title="Login">Login</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+)
+
+export default Nav
