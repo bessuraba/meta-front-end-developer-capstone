ef2d629d50e75491581257463d366fb9c1b4cc69
Slava Varda <slawa.bessuraba@gmail.com>
2025-04-08 21:54:52 +0200

    feat: add mobile styles

--- src/components/footer/Company.jsx
+++ src/components/footer/Company.jsx
@@ -7,8 +7,8 @@ const Company = () => (
       <img className={styles.Image} src="/Logo.svg" alt="logo"/>
     </a>
     <p className={classNames('text-highlight-text', styles.Address)}>789 Citrus Ave, Chicago, IL</p>
-    <p className={classNames('text-highlight-text', styles.Email)}>Email: contact@littlelemonchi.com</p>
-    <p className={classNames('text-highlight-text', styles.Phone)}>Phone: (312) 555-7890</p>
+    <p className={classNames('text-highlight-text', styles.Email)}><span>Email: </span>contact@littlelemonchi.com</p>
+    <p className={classNames('text-highlight-text', styles.Phone)}><span>Phone: </span>(312) 555-7890</p>
   </div>
 )
 
