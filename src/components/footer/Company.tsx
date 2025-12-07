dfdcb8905688b67cafb6d68a89edf8c5708953db
Slava Varda <slawa.bessuraba@gmail.com>
2025-12-07 08:01:33 +0100

    Fix lint errors: Replace <a> with <Link> from next/link

--- src/components/footer/Company.tsx
+++ src/components/footer/Company.tsx
@@ -1,11 +1,12 @@
 import styles from "./Company.module.css"
 import classNames from "classnames"
+import Link from "next/link"
 
 const Company = () => (
   <div className={styles.Wrapper}>
-    <a className={styles.Logo} href="/" title="Little Lemon">
+    <Link className={styles.Logo} href="/" title="Little Lemon">
       <img className={styles.Image} src="/Logo.svg" alt="logo"/>
-    </a>
+    </Link>
     <p className={classNames('text-highlight-text', styles.Address)}>789 Citrus Ave, Chicago, IL</p>
     <p className={classNames('text-highlight-text', styles.Email)}><span>Email: </span>contact@littlelemonchi.com</p>
     <p className={classNames('text-highlight-text', styles.Phone)}><span>Phone: </span>(312) 555-7890</p>
